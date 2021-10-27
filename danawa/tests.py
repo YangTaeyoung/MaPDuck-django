e285d81208097b8ce33f67fe39a7d10c80354f4a
Taeyoung Yang <59782504+YangTaeyoung@users.noreply.github.com>
2021-10-27 21:05:05 +0900

    test: 구동 테스트 추가.

--- danawa/tests.py
+++ danawa/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
-
+from .services.danawa_separator import DanawaSeparator
 # Create your tests here.
+class DanawaSeparatorTest(TestCase):
+    def test_get_model_name(self):
+        ds = DanawaSeparator(dict())
+        self.assertIsNotNone(ds.get_model_name())
+        print(ds.get_model_name())
