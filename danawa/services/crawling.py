4a451bea5f7372eb3eb35a417723014b3a5fea98
anfqlc1127@gmail.com <anfqlc1127@gmail.com>
2021-10-28 00:38:11 +0900

    crawling_update

--- danawa/services/crawling.py
+++ danawa/services/crawling.py
@@ -1,36 +1,86 @@
+#crawling
 import requests
 from bs4 import BeautifulSoup
 
+# json
+import json
+from collections import OrderedDict
+
 class DanawaCrawler:
 
-    # 함수화 해야함...(I'm sorry...)
-    # 소스코드만 올리는데, 이거 장고에서 어떻게 실행해야할지(???)...
-    # PR_NAMES: 제품명 , PR_DESCRIPTION: 제품 리스트
+    # to_json : list to json
+    # product_name_list : 제품명 리스트
+    # product_desc_list : 제품 스펙 리스트
+    # num : len(product_name_list)
+    def to_json(product_name_list, product_desc_list, num):
+        data_list_json = OrderedDict()
+        data_list = []
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
-    # 모니터 검색(예시) , 모니터 부분만 받아서 하면 될듯
-    url = "http://search.danawa.com/dsearch.php?query=모니터&tab=main"
+        for i in range(num):
+            orderdict = OrderedDict()
+            orderdict["prduct_num"] = i
+            orderdict["product_name"] = product_name_list[i]
+            orderdict["description"] = product_desc_list[i]
 
-    resp = requests.get(url, headers=headers)
-    soup = BeautifulSoup(resp.text, "html.parser")
+            data_list.append(orderdict)
 
-    # 제품 리스트
-    PR_LIST = soup.select("#productListArea > div.main_prodlist.main_prodlist_list > ul.product_list > li")
-    PR_NAMES = []  # 제품이름
-    PR_DESCRIPTION = []  # 제품 스펙
+        data_list_json["products"] = data_list
+        json_data = json.dumps(data_list_json, ensure_ascii=False, indent="\t")
 
-    for i in range(len(PR_LIST)):
-        try:
-            PR_NAMES.append(PR_LIST[i].select_one("div > div.prod_info > p > a").text)
+        return json_data
 
-            desc_list = []
-            desc = PR_LIST[i].select("div > dl.prod_spec_set > dd > div > a")
-            for j in range(len(desc)):
-                desc_list.append(desc[j].text)
+    # get_product_list_danawa : 검색 결과 크롤링
+    # search : 검색어
+    # end_page_num : 검색할 마지막 페이지
+    def get_product_list_danawa(search, end_page_num):
 
-            PR_DESCRIPTION.append(desc_list)
-        except:
-            continue
+
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
+        URL = "http://search.danawa.com/dsearch.php?query={}&originalQuery={}&volumeType=allvs&page={}&limit=40&sort=saveDESC&list=list&boost=true&addDelivery=N&recommendedSort=Y&defaultUICategoryCode=112757&defaultPhysicsCategoryCode=860%7C13735%7C14883%7C58972&defaultVmTab=27335&defaultVaTab=1412149&tab=goods"
+
+        # 제품이름
+        product_names = []
+        # 제품 스펙
+        product_desc = []
+        # 페이지 시작 번호
+        page_num = 1
+
+        # crawling start
+        while True:
+
+            # end_page
+            if page_num >= end_page_num:
+                break
+
+            url = URL.format(search, search, page_num)
+            resp = requests.get(url, headers=headers)
+            soup = BeautifulSoup(resp.text, "html.parser")
+
+            # 제품 리스트
+            product_list = soup.select(
+                "#productListArea > div.main_prodlist.main_prodlist_list > ul.product_list > li")
+
+            for i in range(len(product_list)):
+                try:
+                    product_names.append(product_list[i].select_one("div > div.prod_info > p > a").text)
+                    desc_list = []
+                    desc = product_list[i].select("div > dl.prod_spec_set > dd > div > a")
+                    for j in range(len(desc)):
+                        desc_list.append(desc[j].text)
+
+                    product_desc.append(desc_list)
+
+                except:
+                    continue
+
+            page_num += 1
+            # crawling end
+
+        # to_json
+        products_json = DanawaCrawler.to_json(product_names, product_desc, len(product_names))
+
+        return products_json
+
+
 
 
