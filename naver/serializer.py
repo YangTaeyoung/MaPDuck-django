38f328da2dc21af202080097ecd35c4e2bb3cb70
Taeyoung Yang <59782504+YangTaeyoung@users.noreply.github.com>
2021-11-15 19:53:59 +0900

    Fix: 날짜 purchased_at속성 char 필드로 수정하여 스트링 형태로 반환하도록 지정.

--- naver/serializer.py
+++ naver/serializer.py
@@ -2,4 +2,4 @@ from rest_framework import serializers
 
 class MyProductSerializer(serializers.Serializer):
     mo_name = serializers.CharField(max_length=200)
-    purchased_at = serializers.DateTimeField()
+    purchased_at = serializers.CharField()
