d668d8a8940d295ed6f9838618efa329dbbaf7a2
Taeyoung Yang <59782504+YangTaeyoung@users.noreply.github.com>
2021-11-15 19:53:29 +0900

    Fix: warranty 속성 IntegerField로 변경

--- danawa/serializer.py
+++ danawa/serializer.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 
+
 class ProductSerializer(serializers.Serializer):
     co_id = serializers.IntegerField()
     pr_name = serializers.CharField(max_length=30)
     mo_name = serializers.CharField(max_length=30)
     desc = serializers.CharField(max_length=300)
-    warranty = serializers.CharField()
+    warranty = serializers.IntegerField()
     img_path = serializers.CharField(max_length=500)
